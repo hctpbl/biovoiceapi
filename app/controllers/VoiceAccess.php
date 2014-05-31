71d838ffac79017b34372a026af0f5dc3006f932
Héctor <hctpbl@gmail.com>
2014-05-31 19:24:15 +0200

    Started development of AlizePHP facade. Changed composer version of AlizePHP required to "dev-master".

---
+++ app/controllers/VoiceAccess.php
@@ -0,0 +1,12 @@
+<?php
+namespace apibvw\Controller;
+
+use \BaseController;
+use \alizephp;
+
+class VoiceAccess extends BaseController {
+	
+	public function postEnroll($usuario) {
+		
+	}
+}
---
+++ app/speakerrec/AlizePHPFacade.php
@@ -0,0 +1,11 @@
+<?php
+namespace apibvw\SpeakerRec;
+
+use alizephp\AlizePHP;
+
+class AlizePHPFacade implements SpeakerRecognitionPackage {
+	
+	public static function isEnrolled($user) {
+		//$alizephp_user = new AlizePHP($user, $audio_file_path);
+	}
+}
