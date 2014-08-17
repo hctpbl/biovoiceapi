fd02bc3e7b50e1d0d71bc37b2ab0b15c71132b5b
Héctor <hctpbl@gmail.com>
2014-08-17 16:27:48 +0200

    Added config value ti store the threshold for the speaker verification system.

---
+++ app/config/speakerrec.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+		/*
+		 |--------------------------------------------------------------------------
+		 | Threshold
+		 |--------------------------------------------------------------------------
+		 |
+		 | Represents the threshold to use when performing speaker verification. It
+		 | lets systems using this API decide whether an audio sample may belong to
+		 | a user or not.
+		 |
+		 */
+
+		'threshold' => '3',
+
+);
--- app/controllers/VoiceAccess.php
+++ app/controllers/VoiceAccess.php
@@ -2,6 +2,7 @@
 namespace apibvw\Controller;
 
 use \BaseController;
+use \Config;
 use \Input;
 use \Request;
 use \Response;
@@ -53,10 +54,11 @@ class VoiceAccess extends BaseController {
 		
 		$speaker_rec_package = SpeakerRecognitionManager::getSpeakerRecognitionSystem($usuario,
 				SpeakerRecognitionManager::ENGINE_ALIZEPHP);
-		
+
+		$threshold = Config::get('speakerrec.threshold');
 		$result = $speaker_rec_package->enroll($raw_audio_file_path);
 
-		return Response::json(array('error'=>false, 'result' => '0'),200);
+		return Response::json(array('error'=>false, 'threshold'=>$threshold, 'result' => '0'),200);
 	}
 	
 	public function postTest($usuario) {
@@ -65,10 +67,11 @@ class VoiceAccess extends BaseController {
 		
 		$speaker_rec_package = SpeakerRecognitionManager::getSpeakerRecognitionSystem($usuario,
 				SpeakerRecognitionManager::ENGINE_ALIZEPHP);
-		
+
+		$threshold = Config::get('speakerrec.threshold');
 		$result = $speaker_rec_package->testSpeakerIdentity($raw_audio_file_path);
 
-		return Response::json(array('error'=>false, 'result' => $result),200);
+		return Response::json(array('error'=>false, 'threshold'=>$threshold, 'result' => $result),200);
 	}
 	
 	public function getStatus($usuario) {
